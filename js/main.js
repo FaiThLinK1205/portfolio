687795458dac5a750c0d52fc64241425bdfef52a
tanaka-et <lllib.bye2@gmail.com>
2021-01-10 20:17:02 +0900

    更新しました

--- js/main.js
+++ js/main.js
@@ -86,3 +86,30 @@ const spanWrap = new SpanWrap();
 spanWrap.init({
   target: ".fv_txt"
 });
+
+
+// ======= contact ======= //
+$(document).ready(function () {
+
+      $('#form').submit(function (event) {
+        var formData = $('#form').serialize();
+        $.ajax({
+          url: "https://docs.google.com/forms/u/3/d/e/1FAIpQLSfsH5qvk0Oe66DNVX_W9asTbwvkPT2govlRaAyXm-3T4dyXjg/formResponse",
+          data: formData,
+          type: "POST",
+          dataType: "xml",
+          statusCode: {
+            0: function () {
+              $(".end-message").slideDown();
+              $(".submit-btn").fadeOut();
+              //window.location.href = "thanks.html";
+            },
+            200: function () {
+              $(".false-message").slideDown();
+            }
+          }
+        });
+        event.preventDefault();
+      });
+
+    });
